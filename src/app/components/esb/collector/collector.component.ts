1f544c810049e311e7a86366ea60e07435a50471
InvenireAudeAdmin <albert.krzymowski@invenireaude.com>
2018-12-12 18:29:51 +0100

    connector updated.

--- src/app/components/esb/collector/collector.component.ts
+++ src/app/components/esb/collector/collector.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ServiceDirectoryService } from 'src/app/services/esb/service-directory.service';
 import { CollectService } from 'src/app/services/esb/collect.service';
 
-import { NgbDateParserFormatter, NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateParserFormatter, NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import { TypesService } from 'src/app/services/esb/types.service';
 
 export function toInteger(value: any): number {
   return parseInt(`${value}`, 10);
@@ -25,23 +26,27 @@ export function isNumber(value: any): value is number {
 })
 export class CollectorComponent implements OnInit {
 
-  public applications: any[];
+  public applications: any[] = [];
+  public interfaces: any[] = [];
+
   public messages = [];
-  public dsBeginTo:   NgbDateStruct;
+  public dsBeginTo: NgbDateStruct;
   public dsBeginFrom: NgbDateStruct;
 
-  public timeBeginTo    = { hour: 13, minute: 30};
-  public timeBeginFrom  = { hour: 13, minute: 30};
+  public timeBeginTo = { hour: 13, minute: 30 };
+  public timeBeginFrom = { hour: 13, minute: 30 };
 
+  public srcAppl: string = null;
+  public dstAppl: string = null;
 
-  public selector = {
-    tsBeginTo: null,
-    tsBeginFrom: null
-  };
-
+  public interface: any = null;
+  public service: string = null;
+  public refId: string = null;
+  public parentId: string = null;
 
   constructor(
     private serviceDirectoryService: ServiceDirectoryService,
+    private serviceTypesService: TypesService,
     private collectService: CollectService,
     private calendar: NgbCalendar
   ) { }
@@ -54,36 +59,78 @@ export class CollectorComponent implements OnInit {
     const date = new Date();
 
     this.timeBeginTo = { hour: date.getHours() + 1, minute: date.getMinutes() };
-    this.timeBeginFrom = { hour: date.getHours() - 1, minute: date.getMinutes()};
+    this.timeBeginFrom = { hour: date.getHours() - 1, minute: date.getMinutes() };
 
     console.log(JSON.stringify(this.timeBeginFrom));
+
     this.serviceDirectoryService.getApplications().subscribe(
       (data) => {
-          this.applications = data.applications;
+        this.applications = data.applications;
+      });
+
+    this.serviceDirectoryService.getInterfaces().subscribe(
+      (data) => {
+        if (data.interfaces) {
+          this.interfaces = data.interfaces;
+          console.log(this.interfaces);
+          this.interfaces.forEach(i => {
+            this.serviceTypesService.getTypeAsService(i.namespace).subscribe(data1 => {
+              if (data1.response && data1.response.properties) {
+                i.services = data1.response.properties;
+              }
+            });
+          });
+        }
       });
 
   }
 
   makeTimestamp(date: NgbDateStruct, time: any): string {
     return date ?
-        date.year + '-' +
-        padNumber(date.month) + '-' +
-        padNumber(date.day) + 'T' +
-        padNumber(time.hour)  + ':' +
-        padNumber(time.minute) + ':00' :
-        null;
+      date.year + '-' +
+      padNumber(date.month) + '-' +
+      padNumber(date.day) + 'T' +
+      padNumber(time.hour) + ':' +
+      padNumber(time.minute) + ':00' :
+      null;
   }
 
   makeSelector() {
+    const selector: any = {};
 
-    this.selector.tsBeginTo = this.makeTimestamp(this.dsBeginTo, this.timeBeginTo);
-    this.selector.tsBeginFrom = this.makeTimestamp(this.dsBeginFrom, this.timeBeginFrom);
+    selector.tsBeginTo = this.makeTimestamp(this.dsBeginTo, this.timeBeginTo);
+    selector.tsBeginFrom = this.makeTimestamp(this.dsBeginFrom, this.timeBeginFrom);
+
+    if (this.srcAppl) {
+      selector.srcAppl = this.srcAppl;
+    }
+
+    if (this.srcAppl) {
+      selector.srcAppl = this.srcAppl;
+    }
+
+    if (this.interface) {
+      selector.interface = this.interface.id;
+    }
+
+    if (this.service) {
+      selector.service = this.service;
+    }
+
+    if (this.refId) {
+      selector.refId = this.refId;
+    }
+
+    if (this.parentId) {
+      selector.parentId = this.parentId;
+    }
+
+    return selector;
   }
 
   send() {
 
-    this.makeSelector();
-    this.collectService.getMessageHistory(this.selector).subscribe(
+    this.collectService.getMessageHistory(this.makeSelector()).subscribe(
       (data) => {
         if (data.messages !== undefined) {
           this.messages = data.messages;
